5471a6dfa2aedfc41c4699d72fe9199cc12b9463
mateomedus <mateomedus@gmail.com>
2021-06-07 17:30:30 -0300

    index url

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('dashboard.urls', namespace='dashboard')),
+    path('api/', include('dashboard_api.urls', namespace='dashboard_api')),
+]
