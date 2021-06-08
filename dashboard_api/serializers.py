0db5024f85dea42e308b8512e6f48470689b9081
mateomedus <mateomedus@gmail.com>
2021-06-08 16:47:27 -0300

    react working

--- dashboard_api/serializers.py
+++ dashboard_api/serializers.py
@@ -3,5 +3,5 @@ from dashboard.models import Coin
 
 class CoinSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'name', 'price', 'category')
+        fields = ('id', 'name', 'price')
         model = Coin
