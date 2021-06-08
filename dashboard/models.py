18ed8f4085eb5360132d205d0aee721291641260
mateomedus <mateomedus@gmail.com>
2021-06-08 16:47:14 -0300

    api working

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,22 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Coin(models.Model):
 
     class CoinObjects(models.Manager):
         def get_queryset(self):
             return super().get_queryset() 
 
-    category = models.ForeignKey(
-        Category, on_delete=models.PROTECT, default=1)
     name = models.CharField(max_length=250)
     price = models.CharField(max_length=250) 
     objects = models.Manager()  # default manager
