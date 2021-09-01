2015c5bd6e5147e80a864a5461a2eeeea2e953f0
agustinstr <agus.stragua@gmail.com>
2021-08-31 21:55:36 -0300

    Add various features

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login'; 
-import SignUp from './components/SignUp';
+import Register from './components/Register';
 import Logout from './components/Logout';
 import Dashboard from './components/Dashboard';
 
@@ -16,7 +16,7 @@ const routing = (
 			<Header />
 			<Switch>
 				<Route path='/login' component={Login} exact />
-          		<Route path='/signup' component={SignUp} exact />
+          		<Route path='/register' component={Register} exact />
 				<Route path='/logout' component={Logout} exact />
 				<Route path='/dashboard' component={Dashboard} exact />
 				<Route exact path="/" component={App} />
